68414a441375e5ac8a0a3e42393e458fe1d618b7
nahueltome <nahuel.tome2@gmail.com>
2020-08-11 10:35:46 -0300

    validación en js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,37 @@
+function datos(){
+    var i = document.getElementById("nombre").value;
+    var contenido = "";
+    if (i == "" || i == null){
+        contenido += `
+            <p>Debe ingresar un usuario</p>
+        `
+        document.getElementById("errorusuario").innerHTML = contenido;
+        return false
+    }
+    var j = document.getElementById("contraseña").value;
+    var contenido1 = "";
+    if (j == "" || j == null){
+        contenido1 += `
+            <p>Debe ingresar una contraseña</p>
+        `
+        document.getElementById("errorcontraseña").innerHTML = contenido1;
+        return false
+    }
+}
+function validar(){
+    if (datos() == false){
+        return datos();
+    }
+    else {
+        return location.href="index.html";
+    }
+}
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
