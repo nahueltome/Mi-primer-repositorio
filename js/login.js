40f3ae40a1bdeb22412a34fe92fd7a0a9ba9ccb7
nahueltome <nahuel.tome2@gmail.com>
2020-08-12 21:16:38 -0300

    Redirecciona al login sin dar loop

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 function datos(){
-    var i = document.getElementById("nombre").value;
+    var i = document.getElementById("user").value;
     var contenido = "";
     if (i == "" || i == null){
         contenido += `
@@ -8,7 +8,7 @@ function datos(){
         document.getElementById("errorusuario").innerHTML = contenido;
         return false
     }
-    var j = document.getElementById("contraseña").value;
+    var j = document.getElementById("pass").value;
     var contenido1 = "";
     if (j == "" || j == null){
         contenido1 += `
@@ -18,6 +18,10 @@ function datos(){
         return false
     }
 }
+
+var user = sessionStorage.setItem("user", "user");
+var pass = sessionStorage.setItem("pass", "pass");
+
 function validar(){
     if (datos() == false){
         return datos();
@@ -26,6 +30,7 @@ function validar(){
         return location.href="index.html";
     }
 }
+
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
